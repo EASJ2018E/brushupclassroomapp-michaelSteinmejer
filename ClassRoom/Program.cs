2f730bd4e00f89a9b9686b23828ce1167f44c43b
Michael Steinmejer <mich86p0@edu.easj.dk>
2018-09-05 16:27:23 +0200

    Tilføjet klasse og studerende

--- ClassRoom/Program.cs
+++ ClassRoom/Program.cs
@@ -10,6 +10,20 @@ namespace ClassRoom
     {
         static void Main(string[] args)
         {
+            KlasseRum klasse = new KlasseRum();
+
+            Studerende Kevin = new Studerende("Kevin",8,8);
+            Studerende Sæve = new Studerende("Sebastian",7,7);
+            Studerende Michael = new Studerende("Michael", 6, 6);
+
+
+            klasse.Klasseliste.Add(Kevin);
+            klasse.Klasseliste.Add(Sæve);
+            klasse.Klasseliste.Add(Michael);
+
+            klasse.Klassenavn = "3B";
+            klasse.SemesterStart = new DateTime(4-9-2018);
+
         }
     }
 }
